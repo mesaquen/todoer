64c8d9790e90fafcf4aa70bcbb6b6a666ee9dec5
Mesaque Francisco <mesaquenf@gmail.com>
2021-02-16 19:35:33 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Container id="app">
+    <h1>Sample</h1>
+  </Container>
+</template>
+
+<script>
+import Container from "@/components/Container.vue";
+
+export default {
+  name: "App",
+  components: {
+    Container
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container"><slot /></div>
+</template>
+
+<script>
+export default {
+    name: "Container"
+}
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
